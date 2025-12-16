51fcb91ca63dc8df775b183bce9db12ba17f6249
Roque Santos <roque-100@hotmail.com>
2025-12-16 00:29:31 -0300

    added lua config

--- src/main.lua
+++ src/main.lua
@@ -1,9 +1,24 @@
-function love.load() end
+GameState = {
+	fullscreen = false,
+}
 
+function love.load()
+	love.graphics.setDefaultFilter("nearest", "nearest")
+end
+
+---@param dt number
 function love.update(dt) end
 
 function love.draw() end
 
+---@param key string
 function love.keypressed(key)
 	print(key)
+
+	if key == "escape" then
+		love.event.quit()
+	elseif key == "f11" then
+		GameState.fullscreen = not GameState.fullscreen
+		love.window.setFullscreen(GameState.fullscreen)
+	end
 end
